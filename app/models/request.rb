546939118613b00f5c18cf51ab28ed0713272729
lukasz <l3moniak2@gmail.com>
2014-04-26 12:12:15 +0200

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # On branch master # Your branch is ahead of 'origin/master' by 1 commit. #   (use "git push" to publish your local commits) # # Changes to be committed: #   (use "git reset HEAD <file>..." to unstage) # #	modified:   Gemfile #	modified:   Gemfile.lock #	new file:   app/assets/javascripts/offers.js.coffee #	modified:   app/controllers/application_controller.rb #	new file:   app/controllers/offers_controller.rb #	modified:   app/controllers/requests_controller.rb #	new file:   app/decorators/offer_decorator.rb #	new file:   app/helpers/offers_helper.rb #	new file:   app/models/offer.rb #	modified:   app/models/request.rb #	modified:   app/models/user.rb #	modified:   app/views/layouts/application.html.haml #	new file:   app/views/offers/_form.html.haml #	new file:   app/views/offers/edit.html.haml #	new file:   app/views/offers/index.html.haml #	new file:   app/views/offers/new.html.haml #	new file:   app/views/offers/show.html.haml #	modified:   app/views/requests/_form.html.haml #	modified:   app/views/requests/edit.html.haml #	modified:   app/views/requests/index.html.haml #	new file:   app/views/requests/myRequests.html.haml #	modified:   app/views/requests/show.html.haml #	modified:   config/routes.rb #	new file:   db/migrate/20140426084428_create_offers.rb #	new file:   db/migrate/20140426084744_add_request_id_to_offers.rb #	modified:   db/schema.rb #	new file:   spec/controllers/offers_controller_spec.rb #	new file:   spec/decorators/offer_decorator_spec.rb #	new file:   spec/factories/offers.rb #	new file:   spec/models/offer_spec.rb # Added exposure in requests Added one test

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,8 @@
 class Request < ActiveRecord::Base
   belongs_to :user
   has_many :offers
+
+  def offers_count
+    self.offers.count
+  end
 end
--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe Request do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { create(:request) }
+
+  describe '#offers_count' do
+    let(:offer) { create(:offer) }
+    before { subject.offers << offer }
+
+    its(:offers_count) { should eql 1 }
+  end
 end
